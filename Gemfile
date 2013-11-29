a6eb2e973bbebbd22951781e8022b68d64f2c534
Matt Bessey <bessey@gmail.com>
2013-11-29 14:20:57 -0800

    bare sinatra config

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'sinatra'
+gem 'sinatra-assetpack', require: 'sinatra/assetpack'
+gem 'sass'
---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+
+class App < Sinatra::Base
+
+  get "/" do
+    haml :index
+  end
+
+  set :root, File.dirname(__FILE__) # You must set app root
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'js'        # Default
+    serve '/css',    from: 'css'       # Default
+    serve '/images', from: 'images'    # Default
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    js :app, [
+      '/js/app.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/screen.css'
+    ]
+
+    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
+    css_compression :simple   # :simple | :sass | :yui | :sqwish
+  }
+
+end
